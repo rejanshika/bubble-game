970c8f788742378c5f9a9a7a697b559219c57d4d
Anshika <rejanshi1234@gmail.com>
2023-08-23 22:58:29 +0530

    bubble game

---
+++ syntax.js
@@ -0,0 +1,49 @@
+var timer = 60;
+var score = 0;
+var hitrn = 0;
+
+function increaseScrore(){
+    score += 10;
+    document.querySelector("#scoreval").textContent = score;
+}
+
+function getNewHit(){
+    hitrn = Math.floor(Math.random()*10);
+    document.querySelector("#hitval").textContent = hitrn;
+}
+function makeBubble(){
+    
+    var clutter = "";
+
+for(var i = 1; i<=160; i++){
+    var rn = Math.floor(Math.random()*10)
+    clutter += `<div class="bubble">${rn}</div>`;
+}
+
+document.querySelector(".pbtm").innerHTML = clutter;
+}
+function runTimer(){
+     var timerint = setInterval( function(){
+        if( timer > 0){
+            timer--;
+            document.querySelector("#timerval").textContent = timer;
+        }else{
+            clearInterval(timerint);
+            document. querySelector(".pbtm").innerHTML = `<h1>Game over</h1>`
+        }
+    },1000)
+}
+
+document.querySelector(".pbtm").addEventListener("click" ,function(dets){
+    var clickedum = Number(dets.target.textContent);
+    if(clickedum === hitrn){
+        increaseScrore();
+        makeBubble();
+        getNewHit();
+    }
+})
+
+increaseScrore();
+getNewHit();
+runTimer();
+makeBubble();
